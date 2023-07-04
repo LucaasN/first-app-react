085608f1d7e7a759f1268190ed9affc478b42579
Lucas N <lucas_mdq21@hotmail.com>
2023-07-03 23:18:11 -0300

    first commit

---
+++ src/data/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "primer tarea",
+    description: "test primer tarea",
+  },
+  {
+    id: 1,
+    title: "segunda tarea",
+    description: "test segunda tarea",
+  },
+  {
+    id: 2,
+    title: "tercer tarea",
+    description: "test tercer tarea",
+  },
+];
